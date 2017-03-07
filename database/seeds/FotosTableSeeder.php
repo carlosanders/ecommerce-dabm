2712cc810f7dac822098c816313d56cf6c9d1229
Carlos Anders <carlosanders@gmail.com>
2017-03-06 22:26:57 -0300

    Add all products from PDU for tests for ecommerce - Joppert

--- database/seeds/FotosTableSeeder.php
+++ database/seeds/FotosTableSeeder.php
@@ -1,8 +1,6 @@
 <?php
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
 class FotosTableSeeder extends Seeder
 {
     /**
@@ -13,10 +11,8 @@ class FotosTableSeeder extends Seeder
     public function run()
     {
         DB::table('images')->truncate();
-
         $this->createWithDB();
     }
-
     public function createWithDB()
     {
         DB::table("images")->insert(array(
@@ -283,9 +279,267 @@ class FotosTableSeeder extends Seeder
                 'product_id' => 58,
                 'image_path' => '245.JPG',
             ], //58
-
+            //###############################################################                        
+            [
+                'product_id' => 59,
+                'image_path' => '245.JPG',
+            ], //59     
+            [
+                'product_id' => 60,
+                'image_path' => '245.JPG',
+            ], //60            
+            [
+                'product_id' => 61,
+                'image_path' => '245.JPG',
+            ], //61            
+            [
+                'product_id' => 62,
+                'image_path' => '245.JPG',
+            ], //62                                                       
+            //###############################################################  
+            [
+                'product_id' => 63,
+                'image_path' => '246.JPG',
+            ], //63   
+            [
+                'product_id' => 64,
+                'image_path' => '246.JPG',
+            ], //64  
+            [
+                'product_id' => 65,
+                'image_path' => '246.JPG',
+            ], //65  
+            [
+                'product_id' => 66,
+                'image_path' => '246.JPG',
+            ], //66                                      
+            //###############################################################      
+            [
+                'product_id' => 67,
+                'image_path' => '42.JPG',
+            ], //67    
+            [
+                'product_id' => 68,
+                'image_path' => '42.JPG',
+            ], //68
+            [
+                'product_id' => 69,
+                'image_path' => '42.JPG',
+            ], //69  
+            [
+                'product_id' => 70,
+                'image_path' => '42.JPG',
+            ], //70                                                         
+            //###############################################################              
+            [
+                'product_id' => 71,
+                'image_path' => '247.JPG',
+            ], //71 
+            [
+                'product_id' => 72,
+                'image_path' => '247.JPG',
+            ], //72 
+            [
+                'product_id' => 73,
+                'image_path' => '247.JPG',
+            ], //73 
+            [
+                'product_id' => 74,
+                'image_path' => '247.JPG',
+            ], //74             
+            //###############################################################                 
+            [
+                'product_id' => 75,
+                'image_path' => '248.JPG',
+            ], //75               
+            //###############################################################        
+            [
+                'product_id' => 76,
+                'image_path' => '256.JPG',
+            ], //76  
+            [
+                'product_id' => 77,
+                'image_path' => '256.JPG',
+            ], //77  
+            [
+                'product_id' => 78,
+                'image_path' => '256.JPG',
+            ], //78  
+            [
+                'product_id' => 79,
+                'image_path' => '256.JPG',
+            ], //79                                                   
+            //###############################################################      
+            [
+                'product_id' => 80,
+                'image_path' => '257.JPG',
+            ], //80    
+            [
+                'product_id' => 81,
+                'image_path' => '257.JPG',
+            ], //81    
+            [
+                'product_id' => 82,
+                'image_path' => '257.JPG',
+            ], //82                                                                           
+            //###############################################################     
+            [
+                'product_id' => 83,
+                'image_path' => '188.JPG',
+            ], //83                                                                           
+            //############################################################### 
+            [
+                'product_id' => 84,
+                'image_path' => '187.JPG',
+            ], //84                                                                           
+            //############################################################### 
+            [
+                'product_id' => 85,
+                'image_path' => '290.JPG',
+            ], //85                                                                           
+            //##############################################################  
+            [
+                'product_id' => 86,
+                'image_path' => '300.JPG',
+            ], //86          
+            [
+                'product_id' => 87,
+                'image_path' => '300.JPG',
+            ], //87          
+            [
+                'product_id' => 88,
+                'image_path' => '300.JPG',
+            ], //88          
+            [
+                'product_id' => 89,
+                'image_path' => '300.JPG',
+            ], //89                                                                                                               
+            //############################################################## 
+            [
+                'product_id' => 90,
+                'image_path' => '102.JPG',
+            ], //90        
+            [
+                'product_id' => 91,
+                'image_path' => '102.JPG',
+            ], //91      
+            [
+                'product_id' => 92,
+                'image_path' => '102.JPG',
+            ], //92      
+            [
+                'product_id' => 93,
+                'image_path' => '102.JPG',
+            ], //93                                                                                                                        //##############################################################    
+            [
+                'product_id' => 94,
+                'image_path' => '268.JPG',
+            ], //94                                                                                                                        
+            [
+                'product_id' => 95,
+                'image_path' => '268.JPG',
+            ], //95                 
+            [
+                'product_id' => 96,
+                'image_path' => '268.JPG',
+            ], //96                 
+            [
+                'product_id' => 97,
+                'image_path' => '268.JPG',
+            ], //97                 
+            [
+                'product_id' => 98,
+                'image_path' => '268.JPG',
+            ], //98                             
+            //##############################################################       
+            [
+                'product_id' => 99,
+                'image_path' => '270.JPG',
+            ], //99                             
+            [
+                'product_id' => 100,
+                'image_path' => '270.JPG',
+            ], //101                                         
+            [
+                'product_id' => 101,
+                'image_path' => '270.JPG',
+            ], //101                             
+            [
+                'product_id' => 102,
+                'image_path' => '270.JPG',
+            ], //102                             
+            [
+                'product_id' => 103,
+                'image_path' => '270.JPG',
+            ], //103                             
+            //##############################################################    
+            [
+                'product_id' => 104,
+                'image_path' => '101.JPG',
+            ], //104                
+            [
+                'product_id' => 105,
+                'image_path' => '101.JPG',
+            ], //105                
+            [
+                'product_id' => 106,
+                'image_path' => '101.JPG',
+            ], //106                
+            [
+                'product_id' => 107,
+                'image_path' => '101.JPG',
+            ], //107                
+            [
+                'product_id' => 108,
+                'image_path' => '101.JPG',
+            ], //108                
+            [
+                'product_id' => 109,
+                'image_path' => '101.JPG',
+            ], //109                                                                                         
+            //############################################################## 
+            [
+                'product_id' => 110,
+                'image_path' => '300.JPG',
+            ], //110   
+            [
+                'product_id' => 111,
+                'image_path' => '300.JPG',
+            ], //111  
+            [
+                'product_id' => 112,
+                'image_path' => '300.JPG',
+            ], //112  
+            [
+                'product_id' => 113,
+                'image_path' => '300.JPG',
+            ], //113                                                                                                      
+            //##############################################################   
+            [
+                'product_id' => 114,
+                'image_path' => '272.JPG',
+            ], //114 
+            [
+                'product_id' => 115,
+                'image_path' => '272.JPG',
+            ], //115   
+            [
+                'product_id' => 116,
+                'image_path' => '272.JPG',
+            ], //116                                                                                                                       
+            [
+                'product_id' => 117,
+                'image_path' => '272.JPG',
+            ], //117                                                                                                                       
+            [
+                'product_id' => 118,
+                'image_path' => '272.JPG',
+            ], //118                                                                                                                       
+            [
+                'product_id' => 119,
+                'image_path' => '272.JPG',
+            ], //119                                                                                                                       //############################################################
         ));
-
         // Exibe uma informação no console durante o processo de seed
         $this->command->info('Table: "images" ref Facade DB demo created');
     }
